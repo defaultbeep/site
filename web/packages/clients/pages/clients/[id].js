d8aea946fd81b4ff921472d61e3542e4a21a0715
Jonathan Goodman <jonathangoodman@deloitte.co.uk>
2021-05-10 22:15:07 +0100

    Show ind

---
+++ web/packages/clients/pages/clients/[id].js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Client() {
+  return (
+    <div>
+        Client would appear here
+        <div><Link href="/clients"><a>← Back</a></Link></div>
+    </div>
+  )
+}
---
+++ web/packages/clients/pages/clients/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Clients() {
+  return (
+    <div>
+      Your clients
+      <ul>
+        <li><Link href="/clients/a"><a>Client a</a></Link></li>
+        <li><Link href="/clients/b"><a>Client b</a></Link></li>
+      </ul>
+    </div>
+  );
+}
