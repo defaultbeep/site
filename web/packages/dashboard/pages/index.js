13fa4f1334fa5626c28cd069255e82e894bd3782
Jonathan Goodman <jonathangoodman@deloitte.co.uk>
2021-05-10 21:17:32 +0100

    Link to clients

--- web/packages/dashboard/pages/index.js
+++ web/packages/dashboard/pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -5,7 +6,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      Dashboard page
+      <Link href="/clients"><a>Dashboard page</a></Link>
     </div>
   )
 }
