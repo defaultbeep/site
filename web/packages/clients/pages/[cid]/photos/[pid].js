236a1b8ec5f50de5b3d24c237e2d81eb141fd8b6
Jonathan Goodman <jonathangoodman@deloitte.co.uk>
2021-06-09 19:29:47 +0100

    log router

--- web/packages/clients/pages/[cid]/photos/[pid].js
+++ web/packages/clients/pages/[cid]/photos/[pid].js
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 export default function Photo() {
+    const router = useRouter();
+    console.log({ router });
   return (
     <div>
         PHOTO A
